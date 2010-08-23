266e7c86420029413600a71c7e5f25012dc3e3ba
Greg Taylor <gtaylor@duointeractive.com>
2010-08-23 10:15:52 -0400

    Fixing up some unit tests.

--- tests/t_express_checkout.py
+++ tests/t_express_checkout.py
@@ -6,7 +6,9 @@ import api_details
 
 interface = interface_factory.get_interface_obj()
 
-# TODO: implement the paypal account log-in as web-based? somehow implement with a bare-bones python web client so it's programmable?
+# TODO: implement the paypal account log-in as web-based? somehow implement with
+# a bare-bones python web client so it's programmable?
+
 class TestExpressCheckout(unittest.TestCase):
     def setUp(self):
         self.returnurl = 'http://www.paypal.com'
@@ -14,36 +16,31 @@ class TestExpressCheckout(unittest.TestCase):
 
     def test_sale(self):
         """
+        Tests the first part of a sale. At this point, this is a partial unit
+        test. The user has to login to PayPal and approve the transaction,
+        which is not something we have tackled in the unit test yet. So we'll
+        just test the set/get_express_checkout methods.
+        
         A call to `SetExpressCheckoutDetails`.
         A call to `DoExpressCheckoutPayment`.
         A call to `GetExpressCheckoutDetails`.
         """
-        setexp_response = interface.set_express_checkout(paymentrequest_0_amt='10.00', 
+        setexp_response = interface.set_express_checkout(amt='10.00', 
             returnurl=self.returnurl, cancelurl=self.cancelurl, 
-            paymentrequest_0_paymentaction='Order', 
+            paymentaction='Order', 
             email=api_details.EMAIL_PERSONAL)
+
         self.assertTrue(setexp_response)
         token = setexp_response.token
-        print setexp_response
-        print "TOKEN", token
-        print "REDIR URL: https://www.sandbox.paypal.com/webscr??cmd=_express-checkout&token=%s&AMT=%s&CURRENCYCODE=USD&RETURNURL=%s&CANCELURL=%s" % (
-                                                        token, '10.00',
-                                                        self.returnurl,
-                                                        self.cancelurl)
-        print "REDIR2  : %s" % interface.generate_express_checkout_redirect_url(token)
+
         getexp_response = interface.get_express_checkout_details(token)
-        print getexp_response
         
-        """
-        This is where you'd redirect your user's browser to PayPal.
-        
-        https://www.sandbox.paypal.com/webscr
-            ?cmd=_express-checkout&token=tokenValue
-            &AMT=amount
-            &CURRENCYCODE=currencyID
-            &RETURNURL=return_url
-            &CANCELURL=cancel_url
-        """
+        # Redirect your client to this URL for approval.
+        redir_url = interface.generate_express_checkout_redirect_url(token)
+        # Once they have approved your transaction at PayPal, they'll get
+        # directed to the returnurl value you defined in set_express_checkout()
+        # above. This view should then call do_express_checkout_payment() with
+        # paymentaction = 'Sale'. This will finalize and bill. 
 
     def test_authorize_and_delayed_capture(self):
         """
