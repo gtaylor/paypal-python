bc7867bb5fdd5c9457934b8604e354127fc26cf4
Greg Taylor <gtaylor@duointeractive.com>
2010-08-24 15:51:58 -0400

    Adding a countries module with a dict of all paypal-supported countries, along with some methods to do various things to the data.

---
+++ tests/t_countries.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+import unittest
+import interface_factory
+from paypal import countries
+
+class TestCountries(unittest.TestCase):
+
+    def test_is_valid_country_abbrev(self):
+        self.assertEqual(True, countries.is_valid_country_abbrev('US'))
+        self.assertEqual(True, countries.is_valid_country_abbrev('us'))
+        self.assertEqual(False, countries.is_valid_country_abbrev('us', 
+                                                                 case_sensitive=True))
+        
+    def test_get_name_from_abbrev(self):
+        us_fullval = 'United States of America'
+        self.assertEqual(us_fullval, countries.get_name_from_abbrev('US'))
+        self.assertEqual(us_fullval, countries.get_name_from_abbrev('us'))
+        self.assertRaises(KeyError, countries.get_name_from_abbrev, 'us',
+                         case_sensitive=True)
+        
+    def test_get_two_tuple(self):
+        country_tuple = countries.get_two_tuple()
+        is_tuple = isinstance(country_tuple, tuple)
+        self.assertEqual(True, is_tuple)
+
+if __name__ == '__main__':
+    unittest.main()
