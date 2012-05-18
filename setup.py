77294d6a8c9207ce5ae18ad6c78e4c909c72ff28
Greg Taylor <gtaylor@duointeractive.com>
2012-05-18 16:03:50 -0400

    Updating some of the packaging, bumping version to 1.2.0.

--- setup.py
+++ setup.py
@@ -12,32 +12,38 @@ LONG_DESCRIPTION = \
 Payment (Guest), and PayPal Express checkouts."""
 
 CLASSIFIERS = [
-                'Development Status :: 5 - Production/Stable',
-                'Intended Audience :: Developers',
-                'License :: OSI Approved :: Apache Software License',
-                'Natural Language :: English',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python',
-                'Programming Language :: Python :: 2',
-                'Programming Language :: Python :: 3',
-                'Topic :: Software Development :: Libraries :: Python Modules' 
-              ]
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
 
-KEYWORDS = 'paypal nvp'
+required = [
+    'requests',
+]
+
+packages = [
+    'paypal',
+]
 
 setup(name = 'paypal',
-      version = VERSION,
-      description = 'PayPal API implementation in Python.',
-      long_description = LONG_DESCRIPTION,
-      author = 'Pat Collins',
-      author_email = 'pat@burned.com',
-      maintainer = 'Gregory Taylor',
-      maintainer_email = 'gtaylor@duointeractive.com',
-      url = 'http://github.com/duointeractive/paypal-python/',
-      download_url = 'http://pypi.python.org/pypi/paypal/',
-      packages = ['paypal'],
-      platforms = ['Platform Independent'],
-      classifiers = CLASSIFIERS,
-      keywords = KEYWORDS,
-      use_2to3 = True
-     )
+      version=VERSION,
+      description='PayPal API implementation in Python.',
+      long_description=LONG_DESCRIPTION,
+      author= 'Pat Collins',
+      author_email='pat@burned.com',
+      maintainer='Gregory Taylor',
+      maintainer_email='gtaylor@gc-taylor.com',
+      install_requires=required,
+      url='http://github.com/duointeractive/paypal-python/',
+      download_url='http://pypi.python.org/pypi/paypal/',
+      packages=packages,
+      platforms=['Platform Independent'],
+      license='Apache Software License',
+      classifiers=CLASSIFIERS,
+      keywords='paypal nvp',
+ )
